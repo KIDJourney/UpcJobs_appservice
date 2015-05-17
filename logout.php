7ea74e5d1479acf4f7eacd82c04ba7150c9f7c17
KIDJourney <kingdeadfish@qq.com>
2015-05-17 21:09:11 +0800

    add logout

--- logout.php
+++ logout.php
@@ -3,9 +3,10 @@
  * @Author: kidjourney
  * @Date:   2015-05-16 23:50:41
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-05-16 23:57:19
+ * @Last Modified time: 2015-05-17 17:14:54
  */
 require_once("lib/http_common.php");
 
+unset($_SESSION['sql_cache']);
 unset($_SESSION['user_info']);
 redirect();
