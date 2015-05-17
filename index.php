8669a4b569fd8220523fac4fe6be2a96b9c775b2
KIDJourney <kingdeadfish@qq.com>
2015-05-18 00:50:48 +0800

    add active label to nav bar

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
  * @Author: kidjourney
  * @Date:   2015-05-16 23:50:04
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-05-17 17:00:38
+ * @Last Modified time: 2015-05-17 23:48:05
  */
     require_once("lib/http_common.php");
     require_once("lib/index_class.php");
+
+    define("INDEX",'active');
 
     $job = new index_class();
     $job->statuscheck();
--- user.php
+++ user.php
@@ -3,10 +3,10 @@
  * @Author: kidjourney
  * @Date:   2015-05-17 21:30:33
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-05-17 23:08:09
+ * @Last Modified time: 2015-05-17 23:49:18
  */
 require_once("lib/user_class.php");
-
+define("USER","active");
 $job = new user_class();
 $data = $job->getuserinfo();
 require_once("template/common/head.php");
