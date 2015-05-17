bc15a41abd08dbde0b56df8e32b76dd81f595fcf
KIDJourney <kingdeadfish@qq.com>
2015-05-17 21:08:32 +0800

    add sqlcache to session

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
  * @Author: kidjourney
  * @Date:   2015-05-16 23:50:04
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-05-16 23:50:29
+ * @Last Modified time: 2015-05-17 17:00:38
  */
     require_once("lib/http_common.php");
     require_once("lib/index_class.php");
 
     $job = new index_class();
     $job->statuscheck();
+    $job->initcache();
--- lib/index_class.php
+++ lib/index_class.php
@@ -3,9 +3,10 @@
  * @Author: kidjourney
  * @Date:   2015-05-06 11:38:55
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-05-17 00:03:23
+ * @Last Modified time: 2015-05-17 17:00:32
  */
 require_once("http_common.php");
+require_once("db_common.php");
 
 class index_class {
 
@@ -16,6 +17,14 @@ class index_class {
             require_once("template/common/footer.php");
         } else {
             redirect("login.php");
+        }
+    }
+
+    public function initcache(){
+        if (isset($_SESSION['sql_cache'])){
+            return ;
+        } else {
+            $_SESSION['sql_cache'] = getsqlstatus();
         }
     }
 }
