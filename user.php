0f7f6072d0616f89dce1d1bbfa8f3b387c05570b
KIDJourney <kingdeadfish@qq.com>
2015-05-17 23:30:39 +0800

    finish user

---
+++ user.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Author: kidjourney
+ * @Date:   2015-05-17 21:30:33
+ * @Last Modified by:   kidjourney
+ * @Last Modified time: 2015-05-17 23:08:09
+ */
+require_once("lib/user_class.php");
+
+$job = new user_class();
+$data = $job->getuserinfo();
+require_once("template/common/head.php");
+require_once("template/user/user.php");
+require_once("template/common/footer.php");
