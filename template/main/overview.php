4ba0c6baea7f57f00f7351d6657c709531dfbced
KIDJourney <kingdeadfish@qq.com>
2015-05-17 23:29:38 +0800

    finish overview

--- template/main/overview.php
+++ template/main/overview.php
@@ -15,74 +15,24 @@
             </div>
             <div class="col-xs-6 col-sm-3 placeholder">
               <!-- <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail"> -->
-              <h4>company nums</h4>
+              <h4>Company nums</h4>
               <span class="text-muted"><?php echo $_SESSION['sql_cache']['camp_num'];?></span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <!-- <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail"> -->
+              <h4>Company news nums</h4>
+              <span class="text-muted"><?php echo $_SESSION['sql_cache']['camp_new_num'];?></span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <!-- <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail"> -->
+              <h4>News nums</h4>
+              <span class="text-muted"><?php echo $_SESSION['sql_cache']['new_num'];?></span>
             </div>
             <div class="col-xs-6 col-sm-3 placeholder">
               <!-- <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail"> -->
               <h4>Meet nums</h4>
               <span class="text-muted"><?php echo $_SESSION['sql_cache']['meet_num'];?></span>
-            </div>
+            </div>        
           </div>
 
-          <h2 class="sub-header">Section title</h2>
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
         </div>
-      </div>
-    </div>
