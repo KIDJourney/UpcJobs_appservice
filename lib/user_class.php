9037e1b86951a02305ec45ebc9ecd19ea4843587
KIDJourney <kingdeadfish@qq.com>
2015-05-17 23:30:24 +0800

    finish user look through and del , ready to add manage

---
+++ lib/user_class.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @Author: kidjourney
+ * @Date:   2015-05-17 21:30:45
+ * @Last Modified by:   kidjourney
+ * @Last Modified time: 2015-05-17 23:18:07
+ */
+    require_once("db_common.php");
+    require_once("http_common.php");
+
+    class user_class {
+
+        private $mysqliworker;
+        private $page;
+
+        public function __construct()
+        {
+            if (checksession()){                
+            }else{
+                redirect();
+            }
+
+            $this->mysqliworker = sqliconnect();
+
+            if (isset($_GET['method']) and $_GET['method']=='del' and isset($_GET['uid'])){
+                $this->delrow($_GET['uid']);
+                die();
+            }
+        }
+
+        public function getuserinfo(){
+            $res = $this->mysqliworker->query("SELECT * FROM user;");
+            $result = array();
+            while ($row=$res->fetch_assoc()){
+                $result[] = $row;
+            }
+            return $result;
+        }
+
+        public function delrow($usernameID){
+            if ($reslut = $this->mysqliworker->query("DELETE FROM user where usernameID=" . $usernameID)){
+                redirect("user.php");
+            } else {
+                die("Delete failed !");
+            }
+        }
+            
+}
---
+++ template/user/user.php
@@ -0,0 +1,32 @@
+       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">User</h1>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <?php
+                  echo "<tr>";
+                  foreach($data[0] as $keys => $value){
+                    echo "<th>" . $keys . "</th>";
+                  }
+                  echo "<th>Manage</th>";
+                  echo "</tr>";
+                ?>
+              </thead>
+              <tbody>
+                <?php 
+                  foreach($data as $subdata){
+                    echo "<tr>";
+                    foreach($subdata as $keys => $row){
+                      echo "<td>" . $row . "</td>";
+                    }
+                    echo "<td>" . "<a href=" . "\"http://upcexample.sinaapp.com/user.php?method=del&uid=" . 
+                                $subdata['usernameID'] . "\">Del</a>" . "</td>";
+                    echo "</tr>";
+                  }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
