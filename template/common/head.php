ffb5087c3d147e8dfa7f172e159726f8ac98f05b
KIDJourney <kingdeadfish@qq.com>
2015-05-17 23:29:12 +0800

    modify nav sidebar

--- template/common/head.php
+++ template/common/head.php
@@ -43,21 +43,15 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
+            <li class="active"><a href="index.php">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="user.php">User</a></li>
+            <li><a href="#">Manager</a></li>
           </ul>
           <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
+            <li><a href="">Jobs info</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Company info</a></li>
+            <li><a href="">Hiring News</a></li>
+            <li><a href="">Meeting info</a></li>
           </ul>
         </div>
