84a32c58a3e41e1ec1f4705960f37c75ef9642e1
KIDJourney <kingdeadfish@qq.com>
2015-05-26 23:40:21 +0800

    fix jobs class init

--- jobs.php
+++ jobs.php
@@ -3,13 +3,13 @@
  * @Author: kidjourney
  * @Date:   2015-05-17 21:30:33
  * @Last Modified by:   kidjourney
- * @Last Modified time: 2015-05-19 00:04:01
+ * @Last Modified time: 2015-05-26 23:35:45
  */
 require_once("lib/data_class.php");
 define("TABLENAME","Jobs");
 
 define("JOBS","active");
-$job = new data_class("job_tb");
+$job = new data_class("profession_information");
 $data = $job->getinfo();
 require_once("template/common/head.php");
 require_once("template/common/data.php");
