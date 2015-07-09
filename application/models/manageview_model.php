0afbad801262456d2f7ee269fade638a62a67eba
KIDJourney <kingdeadfish@qq.com>
2015-07-09 16:50:31 -0400

    add model function for user

--- application/models/manageview_model.php
+++ application/models/manageview_model.php
@@ -5,18 +5,33 @@ class manageview_model extends  CI_Model{
     {
         parent::__construct();
         $this->load->database();
+        $this->load->driver('cache');
     }
 
     /*
      * return the count that overview need */
-    function get_overview(){
+    function get_overview()
+    {
+        $overview_cache = $this->cache->memcached->get("overview");
+        if ($overview_cache) {
+            return $overview_cache;
+        }
         $user_num = $this->db->count_all('user');
         $guide_num = $this->db->count_all('guide');
         $job_num = $this->db->count_all('job');
         $meeting_num = $this->db->count_all('meeting');
-        return array(   "user_num" => $user_num ,
-                        "guide_num" => $guide_num,
-                        "job_num"   => $job_num ,
-                        "meeting_num" => $meeting_num);
+        $overview_data = array(   "user_num" => $user_num ,
+            "guide_num" => $guide_num,
+            "job_num"   => $job_num ,
+            "meeting_num" => $meeting_num);
+        $this->cache->memcached->save('overview',$overview_data,10*60);
+        return $overview_data;
     }
+
+    function get_userinfo()
+    {
+        $user_info = $this->db->get('user');
+        return $user_info->result();
+    }
+
 }
