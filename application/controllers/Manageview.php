5661ee6d4b8904e84f7fbe68fc9c96a87b48b16c
KIDJourney <kingdeadfish@qq.com>
2015-07-09 16:50:18 -0400

    basicly finish user

--- application/controllers/Manageview.php
+++ application/controllers/Manageview.php
@@ -19,37 +19,49 @@ class Manageview extends CI_Controller{
 
     function overview()
     {
-        $overviewdata = $this->manageview_model->get_overview();
+        $overview_data = $this->manageview_model->get_overview();
+        $active_tag = array("overview" =>true);
         $this->load->view('common/head',array('title'=>'Overview'));
         $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
-        $this->load->view('manageview/sidebar');
-        $this->load->view('manageview/overview',$overviewdata);
+        $this->load->view('manageview/sidebar',$active_tag);
+        $this->load->view('manageview/overview',$overview_data);
         $this->load->view('common/footer');
     }
 
     function user()
     {
-
+        $user_data = $this->manageview_model->get_userinfo();
+        $active_tag = array("user"=>true);
+        $this->load->view("common/head",array('title'=>"Manage User"));
+        $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
+        $this->load->view('manageview/sidebar',$active_tag);
+        $this->load->view('manageview/data.php',array("data"=>$user_data,"tablename"=>"user"));
     }
 
     function manager()
     {
-
+        echo "This is manage page";
     }
 
-    function news()
+    function job()
     {
-
+        echo "This is job page";
     }
 
-    function company()
+    function meeting()
     {
-
+        echo "This is meeting page";
     }
+
+    function guide()
+    {
+        echo "This is guide page";
+    }
+
 
     function debug()
     {
-        $data = $this->manageview_model->get_overview();
+        $data = $this->manageview_model->get_userinfo();
         print_r($data);
     }
 
