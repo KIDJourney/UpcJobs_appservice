f7bc4050873411eaa8aa4fbebd5a808b3f3a25a0
KIDJourney <kingdeadfish@qq.com>
2015-07-09 16:50:42 -0400

    modify sidebar

--- application/views/manageview/sidebar.php
+++ application/views/manageview/sidebar.php
@@ -1,15 +1,15 @@
 <div class="container-fluid">
+    <link href="../../../static/css/sidebar.css" rel="stylesheet">
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
             <ul class="nav nav-sidebar">
-                <li class="active"><a href="overview">Overview <span class="sr-only">(current)</span></a></li>
-                <li class=""><a href="user.php">User</a></li>
-                <li class=""><a href="manager.php">Manager</a></li>
+                <li class="<?php echo isset($overview)?"active":"";?>"><a href="overview">Overview <span class="sr-only">(current)</span></a></li>
+                <li class="<?php echo isset($user)?"active":"";?>"><a href="user">User</a></li>
+                <li class="<?php echo isset($manage)?"active":"";?>"><a href="manager">Manager</a></li>
             </ul>
             <ul class="nav nav-sidebar">
-                <li class=""><a href="news.php">News</a></li>
-                <li class=""><a href="company.php">Company info</a></li>
-                <li class=""><a href="hiring.php">Hiring News</a></li>
-                <li class=""><a href="meet.php">Meeting info</a></li>
+                <li class="<?php echo isset($guide)?"active":"";?>"><a href="guide">Guide</a></li>
+                <li class="<?php echo isset($job)?"active":"";?>"><a href="job">Job</a></li>
+                <li class="<?php echo isset($meeting)?"active":"";?>"><a href="meeting">Meeting info</a></li>
             </ul>
         </div>
