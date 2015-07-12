d7a88207e368745dbb68091abda38f6f00752934
KIDJourney <kingdeadfish@qq.com>
2015-07-12 09:21:50 -0400

    add edit config

--- application/controllers/Manageview.php
+++ application/controllers/Manageview.php
@@ -35,38 +35,38 @@ class Manageview extends CI_Controller{
         $this->load->view("common/head",array('title'=>"Manage User"));
         $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
         $this->load->view('manageview/sidebar',$active_tag);
-        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"user"));
+        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"user","edit_type" =>"user"));
     }
 
     function manager()
     {
-        $data = $this->manageview_model_get()->get_managerinfo();
+        $data = $this->manageview_model->get_managerinfo();
         $active_tag = array("manager"=>true);
         $this->load->view("common/head",array('title'=>"Manage admin"));
         $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
         $this->load->view('manageview/sidebar',$active_tag);
-        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"Admin"));
+        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"Admin","edit_type" => "manager"));
     }
 
     function job()
     {
-        $data = $this->manageview_model_get()->get_jobinfo();
+        $data = $this->manageview_model->get_jobinfo();
         $active_tag = array("manager"=>true);
         $this->load->view("common/head",array('title'=>"Manage Jobs"));
         $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
         $this->load->view('manageview/sidebar',$active_tag);
-        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"Jobs"));
+        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"Jobs","edit_type"=>"job"));
 
     }
 
     function meeting()
     {
-        $data = $this->manageview_model_get()->get_meetinginfo();
+        $data = $this->manageview_model->get_meetinginfo();
         $active_tag = array("manager"=>true);
         $this->load->view("common/head",array('title'=>"Manage Meeting"));
         $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
         $this->load->view('manageview/sidebar',$active_tag);
-        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"Jobs"));
+        $this->load->view('manageview/data.php',array("data"=>$data,"tablename"=>"Jobs","edit_type"=>meeting));
     }
 
     function guide()
@@ -76,7 +76,9 @@ class Manageview extends CI_Controller{
 
     function edit($type , $id)
     {
-        echo "You are editing $type , with $id";
+        $this->load->view("common/head",array('title'=>"Editing"));
+        $this->load->view('manageview/topbar',array('username'=>$this->auth_lib->get_username()));
+        $this->load->view('manageview/edit',array("type"=>$type));
     }
     function debug()
     {
