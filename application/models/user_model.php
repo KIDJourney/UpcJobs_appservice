ee560e27c3e6b5140a7a93ae5120e1e1ece94954
KIDJourney <kingdeadfish@qq.com>
2015-07-15 16:21:18 -0400

    user login ready

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,14 +3,15 @@
 
         function __construct()
         {
-            parend::__construct();
+            parent::__construct();
             $this->load->database();
         }
 
-
-
-
-
-
+        public function get_passwd($username)
+        {
+            $result = $this->db->get_where('user',
+                array('user_name'=>$username))->result();
+            return $result?$result['0']->user_passwd:array();
+        }
 
     }
