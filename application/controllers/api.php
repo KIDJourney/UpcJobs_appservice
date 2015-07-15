53dd46a675fe60069f306e81909d81c9f48d4d54
KIDJourney <kingdeadfish@qq.com>
2015-07-15 12:01:20 -0400

    modfiy text to json

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -13,31 +13,35 @@ class Api extends CI_Controller{
         if (!in_array($keyword,$keywordlist) | (!isset($content))){
             show_404();
         }
-        $this->output->set_content_type("application/json");
-        echo json_encode($this->api_model->get_job_info($keyword,$content));
+        $this->output
+             ->set_content_type("application/json")
+             ->set_output(json_encode($this->api_model->get_job_info($keyword,$content)));
     }
 
     function meeting($page = 1)
     {
         if ($page <= 0)
             show_404();
-        $this->output->set_content_type('application/json');
-        echo json_encode($this->api_model->get_meeting_info($page));
+        $this->output
+             ->set_content_type('application/json')
+             ->set_output(json_encode($this->api_model->get_meeting_info($page)));
     }
 
     function news($page = 1)
     {
         if ($page <= 0)
             show_404();
-        $this->output->set_content_type('application/json');
-        echo json_encode($this->api_model->get_news_info($page));
+        $this->output
+             ->set_content_type('application/json')
+             ->set_output(json_encode($this->api_model->get_news_info($page)));
     }
 
     function guide($page = 1)
     {
         if ($page <= 0)
             show_404();
-        $this->output->set_content_type('application/json');
-        echo json_encode($this->api_model->get_guide_info($page));
+        $this->output
+             ->set_content_type('application/json')
+             ->set_output(json_encode($this->api_model->get_guide_info($page)));
     }
 }
