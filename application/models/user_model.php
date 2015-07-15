cf7ad43347d98b11dd308cda60b202a347f6ec31
KIDJourney <kingdeadfish@qq.com>
2015-07-15 16:18:14 -0400

    add user_model

---
+++ application/models/user_model.php
@@ -0,0 +1,16 @@
+<?php
+    class User_model extends  CI_Model{
+
+        function __construct()
+        {
+            parend::__construct();
+            $this->load->database();
+        }
+
+
+
+
+
+
+
+    }
