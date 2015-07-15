2d8c2ecf5d6066c90e89a96d82a68e51601fb81b
KIDJourney <kingdeadfish@qq.com>
2015-07-15 16:18:05 -0400

    modify view

--- application/views/manageview/data.php
+++ application/views/manageview/data.php
@@ -1,34 +1,33 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+<style type="text/css">
+    #edit {
+        margin-left: 5%;
+        margin-right: 10%;
+    }
+    #button-group{
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+</style>
     <h1 class="page-header" style="display:inline-block;"><?php echo $tablename?></h1>
     <a href="<?php echo base_url('manageview/add/' . $edit_type);?>" class="btn btn-info btn-lg" style="float:right;">Add new <?php echo $edit_type;?></a>
     <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
+         <table class="table table-striped">
             <?php
-            echo "<tr>";
-            foreach($data[0] as $keys => $value){
-                echo "<th>" . $keys . "</th>";
-            }
-            echo "<th>Manage</th>";
-            echo "</tr>";
-            ?>
-            </thead>
-            <tbody>
-            <?php
-            foreach($data as $subdata){
-                echo "<tr>";
-                foreach($subdata as $keys => $row){
-                    echo "<td>" . $row . "</td>";
+                echo '<dl class="dl-horizontal">';
+                foreach($data as $subdata) {
+                    echo "<div>";
+                    foreach ($subdata as $keys => $row) {
+                        echo "<dt>" . $keys . "</dt>";
+                        echo "<dd>" . $row . "</dd>";
+                    }
+                    echo "<div id=\"button-group\">" ;
+                    echo "<a class=\"btn btn-primary btn-sm\" id=\"edit\" style=\"display: inline-block;\" href=\"" . base_url("manageview/edit/" . $edit_type . "/" . $subdata->id) ."\">Edit</a>" ;
+                    echo "<a class=\"btn btn-danger btn-sm\"href=\"" . base_url("manageview/delete/" . $edit_type . "/" . $subdata->id) ."\">Delete</a>";
+                    echo "</div>";
+                    echo "</div>";
                 }
-                echo "<td>";
-                echo "<a href=\"" . base_url("manageview/edit/" . $edit_type . "/" . $subdata->id) ."\">Edit</a>";
-                echo "<br>";
-                echo "<a href=\"" . base_url("manageview/delete/" . $edit_type . "/" . $subdata->id) ."\">Delete</a>";
-                echo "</td>";
-                echo "</tr>";
-            }
             ?>
-            </tbody>
         </table>
     </div>
 </div>
